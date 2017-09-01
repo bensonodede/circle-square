fcd58c489a29b43b26d78bf5c5d4292b915abbaa
Benson Odede <Benson Odede>
2017-09-01 15:29:55 +0300

    Updated upload page

--- public/js/upload.js
+++ public/js/upload.js
@@ -23,3 +23,22 @@ $("#done").click(function() {
 
   }
 });
+
+
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  percentPosition: true
+});
+
+$(document).ready(function() {
+  $(function() {
+    $('.lazy').lazy({
+
+      // called after an element was successfully handled
+      afterLoad: function(element) {
+        $grid.masonry();
+      },
+
+    });
+  });
+});
